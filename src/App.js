12e10c4cb63bfe6b3caa896292b3d2816d0e660d
seifelhady22 <elhadyseif22@gmail.com>
2025-12-12 10:48:04 -0800

    add workflow file

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Hello GitHub Actions
+          Learn React
         </a>
       </header>
     </div>
