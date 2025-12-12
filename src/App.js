80764407f6ccd3f1c6d9387317c40eeb8331ec65
seifelhady22 <elhadyseif22@gmail.com>
2025-12-12 13:33:49 -0800

    change test link

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Seif Elhady 🚀
+          to go react page click here 
         </a>
       </header>
     </div>
